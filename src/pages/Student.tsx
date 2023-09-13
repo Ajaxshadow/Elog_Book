482e4fc6981094c8ceb9f9509de2df136a72cfde
JohnsonDaniyan <37460219+JohnsonDaniyan@users.noreply.github.com>
2023-09-13 05:52:47 +0100

    !fix: Date Issue and Week Report Delivery

--- src/pages/Student.tsx
+++ src/pages/Student.tsx
@@ -1,152 +1,152 @@
 import {
-  AiFillLeftCircle,
-  AiFillLeftSquare,
-  AiFillRightCircle,
-  AiFillRightSquare,
-} from "react-icons/ai";
-import React, { useEffect, useState } from "react";
-import StudentSheet, { WeekReport } from "../components/StudentSheet";
+	AiFillLeftCircle,
+	AiFillLeftSquare,
+	AiFillRightCircle,
+	AiFillRightSquare,
+} from 'react-icons/ai';
+import React, { useEffect, useState } from 'react';
+import StudentSheet, { WeekReport } from '../components/StudentSheet';
 
-import { GET_DOCUMENT } from "../hooks/firestoreHooks";
-import { InfinitySpin } from "react-loader-spinner";
-import Particulars from "../components/Particulars";
-import { ParticularsInterface } from "../interface/particulars";
-import WeekSheetMotion from "../components/WeekSheetMotion";
-import { setParticulars } from "../features/particulars/particularsSlice";
-import { useAppSelector } from "../app/hooks";
-import { useDispatch } from "react-redux";
+import { GET_DOCUMENT } from '../hooks/firestoreHooks';
+import { InfinitySpin } from 'react-loader-spinner';
+import Particulars from '../components/Particulars';
+import { ParticularsInterface } from '../interface/particulars';
+import WeekSheetMotion from '../components/WeekSheetMotion';
+import { setParticulars } from '../features/particulars/particularsSlice';
+import { useAppSelector } from '../app/hooks';
+import { useDispatch } from 'react-redux';
 
 const dates: string[] = [
-  "Week 1",
-  "Week 2",
-  "Week 3",
-  "Week 4",
-  "Week 5",
-  "Week 6",
-  "Week 7",
-  "Week 8",
-  "Week 9",
-  "Week 10",
-  "Week 11",
-  "Week 12",
+	'Week 1',
+	'Week 2',
+	'Week 3',
+	'Week 4',
+	'Week 5',
+	'Week 6',
+	'Week 7',
+	'Week 8',
+	'Week 9',
+	'Week 10',
+	'Week 11',
+	'Week 12',
 ];
 
 export default function Student() {
-  const dispatch = useDispatch();
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [move, setMove] = useState(0);
-  const [shouldRenderParticulars, setShouldRenderParticulars] = useState(false);
-  const user = useAppSelector((state) => state.app.user);
-  const particulars = useAppSelector((state) => state.particulars.particulars);
-  const firstTime = useAppSelector((state) => state.app.firstTime);
-  const [weekData, setWeekData] = useState({});
-  const getDoc = async () => {
-    if (user) {
-      const doc = await GET_DOCUMENT("students", user.uid);
-      if (doc?.exists()) {
-        const particularsDB: ParticularsInterface = doc.data().PARTICULARS;
-        const weeklyProgress: WeekReport = doc.data().WEEKLY_PROGRESS;
+	const dispatch = useDispatch();
+	const [currentSlide, setCurrentSlide] = useState(0);
+	const [move, setMove] = useState(0);
+	const [shouldRenderParticulars, setShouldRenderParticulars] =
+		useState(false);
+	const user = useAppSelector(state => state.app.user);
+	const particulars = useAppSelector(state => state.particulars.particulars);
+	const firstTime = useAppSelector(state => state.app.firstTime);
+	const [weekData, setWeekData] = useState({});
+	const getDoc = async () => {
+		if (user) {
+			const doc = await GET_DOCUMENT('students', user.uid);
+			if (doc?.exists()) {
+				const particularsDB: ParticularsInterface =
+					doc.data().PARTICULARS;
+				const weeklyProgress: WeekReport = doc.data().WEEKLY_PROGRESS;
 
-        console.log(user.uid);
-        console.log({ particularsDB, weeklyProgress });
+				console.log(user.uid);
+				console.log({ particularsDB, weeklyProgress });
 
-        if (!particularsDB) {
-          setShouldRenderParticulars(true);
-        } else {
-          //
-          //** Change Back to false 
-          //
-          setShouldRenderParticulars(false);
-          dispatch(setParticulars(particularsDB));
-        }
-        setWeekData(doc.data().WEEKLY_PROGRESS);
-      } else {
-        // docSnap.data() will be undefined in this case
-        console.log("No such document!");
-      }
-    }
-  };
+				if (!particularsDB) {
+					setShouldRenderParticulars(true);
+				} else {
+					//
+					//** Change Back to false
+					//
+					setShouldRenderParticulars(true);
+					dispatch(setParticulars(particularsDB));
+				}
+				setWeekData(doc.data().WEEKLY_PROGRESS);
+			} else {
+				// docSnap.data() will be undefined in this case
+				console.log('No such document!');
+			}
+		}
+	};
 
-  useEffect(() => {
-    getDoc();
-  }, []);
+	useEffect(() => {
+		getDoc();
+	}, []);
 
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const loadData = async () => {
-      await new Promise((r) => setTimeout(r, 2000));
-      setLoading((loading) => !loading);
-    };
-    loadData();
-  }, []);
-  if (loading) {
-    return (
-      <div className=" w-full h-[90vh] grid place-items-center">
-        <InfinitySpin color="#FF4A1C" />
-      </div>
-    );
-  }
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		const loadData = async () => {
+			await new Promise(r => setTimeout(r, 2000));
+			setLoading(loading => !loading);
+		};
+		loadData();
+	}, []);
+	if (loading) {
+		return (
+			<div className=" w-full h-[90vh] grid place-items-center">
+				<InfinitySpin color="#FF4A1C" />
+			</div>
+		);
+	}
 
-  return (
-    <div className="h-[90vh] overflow-hidden gap-5 flex justify-center items-center">
-      {shouldRenderParticulars && <Particulars />}
-      <div
-        className=" cursor-pointer z-50 group"
-        onClick={() => {
-          if (currentSlide <= 0) {
-            setCurrentSlide(dates.length - 1);
-          } else {
-            setCurrentSlide(currentSlide - 1);
-          }
-          setMove(-1);
-        }}
-      >
-        <AiFillLeftSquare
-          size={80}
-          className=" stroke-black stroke-[5px] rounded-lg  text-black/10 group-hover:text-[#FF4A1C] group-hover:stroke-0 transition-colors"
-        />
-        <p className="w-full text-center text-xs font-extrabold group-hover:text-[#FF4A1C] transition-colors">
-          Prev Week
-        </p>
-      </div>
-      <div className=" w-2/3 lg:w-2/6 h-full relative">
-        {dates.map((date, index) => {
-          return particulars?.startDate ? (
-            <StudentSheet
-              weekData={weekData}
-              dates={dates}
-              date={date}
-              key={index}
-              weekID={index}
-              startDate={particulars.startDate}
-              currentIndex={currentSlide}
-              move={move}
-            />
-          ) : (
-            <></>
-          );
-        })}
-      </div>
-      <div
-        className=" cursor-pointer  z-50 group"
-        onClick={() => {
-          if (currentSlide >= dates.length - 1) {
-            setCurrentSlide(0);
-          } else {
-            setCurrentSlide(currentSlide + 1);
-          }
+	return (
+		<div className="h-[90vh] overflow-hidden gap-5 flex justify-center items-center">
+			{shouldRenderParticulars && <Particulars />}
+			<div
+				className=" cursor-pointer z-50 group"
+				onClick={() => {
+					if (currentSlide <= 0) {
+						setCurrentSlide(dates.length - 1);
+					} else {
+						setCurrentSlide(currentSlide - 1);
+					}
+					setMove(-1);
+				}}>
+				<AiFillLeftSquare
+					size={80}
+					className=" stroke-black stroke-[5px] rounded-lg  text-black/10 group-hover:text-[#FF4A1C] group-hover:stroke-0 transition-colors"
+				/>
+				<p className="w-full text-center text-xs font-extrabold group-hover:text-[#FF4A1C] transition-colors">
+					Prev Week
+				</p>
+			</div>
+			<div className=" w-2/3 lg:w-2/6 h-full relative">
+				{dates.map((date, index) => {
+					return particulars?.startDate ? (
+						<StudentSheet
+							weekData={weekData}
+							dates={dates}
+							date={date}
+							key={index}
+							weekID={index}
+							startDate={particulars.startDate}
+							currentIndex={currentSlide}
+							move={move}
+						/>
+					) : (
+						<></>
+					);
+				})}
+			</div>
+			<div
+				className=" cursor-pointer  z-50 group"
+				onClick={() => {
+					if (currentSlide >= dates.length - 1) {
+						setCurrentSlide(0);
+					} else {
+						setCurrentSlide(currentSlide + 1);
+					}
 
-          setMove(1);
-        }}
-      >
-        <AiFillRightSquare
-          size={80}
-          className=" stroke-black stroke-[5px] rounded-lg  text-black/10 group-hover:text-[#FF4A1C] group-hover:stroke-0 transition-colors"
-        />
-        <p className="w-full text-center text-xs font-extrabold group-hover:text-[#FF4A1C] transition-colors">
-          Next Week
-        </p>
-      </div>
-    </div>
-  );
+					setMove(1);
+				}}>
+				<AiFillRightSquare
+					size={80}
+					className=" stroke-black stroke-[5px] rounded-lg  text-black/10 group-hover:text-[#FF4A1C] group-hover:stroke-0 transition-colors"
+				/>
+				<p className="w-full text-center text-xs font-extrabold group-hover:text-[#FF4A1C] transition-colors">
+					Next Week
+				</p>
+			</div>
+		</div>
+	);
 }
--- src/pages/Supervisor.tsx
+++ src/pages/Supervisor.tsx
@@ -1,165 +1,290 @@
 import {
-  Calendar,
-  CalendarControls,
-  CalendarDate,
-  CalendarDays,
-  CalendarMonth,
-  CalendarMonthName,
-  CalendarMonths,
-  CalendarNextButton,
-  CalendarPrevButton,
-  CalendarWeek,
-} from 'datepicker-custum2'
+	Calendar,
+	CalendarControls,
+	CalendarDate,
+	CalendarDays,
+	CalendarMonth,
+	CalendarMonthName,
+	CalendarMonths,
+	CalendarNextButton,
+	CalendarPrevButton,
+	CalendarWeek,
+} from 'datepicker-custum2';
 import { GET_DOCUMENT, useFireHook } from '../hooks/firestoreHooks';
+import {
+	addDays,
+	daysToWeeks,
+	differenceInBusinessDays,
+	parse,
+	parseJSON,
+} from 'date-fns';
 
 import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { CalendarTheme } from '../theme/CalendarTheme';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
 import { DocumentData } from 'firebase/firestore/lite';
-import React from 'react'
-import { addDays } from 'date-fns';
+import React from 'react';
 import { useAppSelector } from '../app/hooks';
 
-const ProgressBar = ({prg,selected}:{prg:number,selected:boolean}) => {
-    return(
-    <div className='flex w-full flex-col-reverse items-center '>
-        <div className={`rounded-lg w-full h-2  ${selected?"bg-white/50":"bg-[#FF4A1C]/20"}`}>
-            <div className={`rounded-lg bg-[#FF4A1C] h-full ${selected?"bg-white":"bg-[#FF4A1C]"}`} style={{width:prg+"%"}}>
-                
-            </div>
-        </div> 
-        <span className={` w-full text-sm font-bold ${selected?"text-white":"text-black/50"}`}>{prg+"%"}</span>
-    </div>
-)}
+const ProgressBar = ({ prg, selected }: { prg: number; selected: boolean }) => {
+	return (
+		<div className="flex w-full flex-col-reverse items-center ">
+			<div
+				className={`rounded-lg w-full h-2  ${
+					selected ? 'bg-white/50' : 'bg-[#FF4A1C]/20'
+				}`}>
+				<div
+					className={`rounded-lg bg-[#FF4A1C] h-full ${
+						selected ? 'bg-white' : 'bg-[#FF4A1C]'
+					}`}
+					style={{ width: prg + '%' }}></div>
+			</div>
+			<span
+				className={` w-full text-sm font-bold ${
+					selected ? 'text-white' : 'text-black/50'
+				}`}>
+				{prg + '%'}
+			</span>
+		</div>
+	);
+};
 
-const Initials = ({name}:{name:string}) =>{
-    const [init] = React.useState({first:name.split(" ")[0][0],last:name.split(" ")[1][0]})
-    return(
-        <div className=' h-10 w-10 bg-[#FF4A1C] rounded-full relative'>
-            <p className=' absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-white'>
-                {`${init.first}${init.last}`}
-            </p>
-        </div>
-    )
-}
+const Initials = ({ name }: { name: string }) => {
+	const [init] = React.useState({
+		first: name.split(' ')[0][0],
+		last: name.split(' ')[1][0],
+	});
+	return (
+		<div className=" h-10 w-10 bg-[#FF4A1C] rounded-full relative">
+			<p className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-white">
+				{`${init.first}${init.last}`}
+			</p>
+		</div>
+	);
+};
 
 export default function Supervisor() {
-    
-    const user = useAppSelector((state) => state.app.user);
-    const {getSupees,getProg, getDaysLeft} = useFireHook();
-    const [sps, setSupees] = React.useState<any[]>()
-    const [prg, setPrg] = React.useState<any[]>([])
-    const [daysL, setDaysL] = React.useState<any[]>([])
-    const [selsectedStudent, setSelectedStudent] = React.useState("")
-    const [highlightedDay, setHighlightedDay] = React.useState<CalendarDate | undefined>()
-    const [startandendDate, setStartandEndDate] = React.useState<{start:Date,end:Date}>()
-    const [selsectedStudentData, setSelectedStudentData] = React.useState<DocumentData>()
-    
-    const getStuff = async () => {if(user){setSupees(await getSupees(user.uid))}}
-    const getPrg = async () => {if(sps){setPrg(await getProg(sps))}}
-    const getDays = async () => {if(sps){setDaysL(await getDaysLeft(sps))}}
-      
-    const selectStud = (uid:string) => {
-        if(selsectedStudent === uid){
-            setSelectedStudent("")
-            setSelectedStudentData(undefined)
-            return null
-        }
-        setSelectedStudent(uid)
-        GET_DOCUMENT("students",uid).then((r)=>{
-            if(r){
+	const user = useAppSelector(state => state.app.user);
+	const { getSupees, getProg, getDaysLeft } = useFireHook();
+	const [sps, setSupees] = React.useState<any[]>();
+	const [prg, setPrg] = React.useState<any[]>([]);
+	const [daysL, setDaysL] = React.useState<any[]>([]);
+	const [selsectedStudent, setSelectedStudent] = React.useState('');
+	const [dayEntryExists, setDayEntryExists] = React.useState(false);
+	const [whatWeek, setWhatWeek] = React.useState<{
+		weekNum: number;
+		dayName: string;
+	}>();
+	const [highlightedDay, setHighlightedDay] = React.useState<
+		CalendarDate | undefined
+	>();
+	const [startandendDate, setStartandEndDate] = React.useState<{
+		start: Date;
+		end: Date;
+	}>();
+	const [selsectedStudentData, setSelectedStudentData] =
+		React.useState<DocumentData>();
 
-                setSelectedStudentData(r.data())
-                const sD = new Date(r.data().PARTICULARS.startDate)
-                setStartandEndDate({start:sD,end:addDays(sD,84)})
-            }
-        })
-    }
+	const getStuff = async () => {
+		if (user) {
+			setSupees(await getSupees(user.uid));
+		}
+	};
+	const getPrg = async () => {
+		if (sps) {
+			setPrg(await getProg(sps));
+		}
+	};
+	const getDays = async () => {
+		if (sps) {
+			setDaysL(await getDaysLeft(sps));
+		}
+	};
 
-    React.useEffect(()=>{
-        
-        !sps&&getStuff();
-        if(!prg||prg.length<1){
-            getPrg()
-        }
-        if(!daysL||daysL.length<1){
-            getDays()
-        }
-        console.log({sps,prg,daysL})
-    },[getProg,getDays,sps,prg])
+	const selectStud = (uid: string) => {
+		if (selsectedStudent === uid) {
+			setSelectedStudent('');
+			setSelectedStudentData(undefined);
+			return null;
+		}
+		setSelectedStudent(uid);
+		GET_DOCUMENT('students', uid).then(r => {
+			if (r) {
+				setSelectedStudentData(r.data());
+				const sD = new Date(r.data().PARTICULARS.startDate);
+				setStartandEndDate({ start: sD, end: addDays(sD, 84) });
+			}
+		});
+	};
 
-    
-    return (
-        <div className='mt-10 flex gap-3 px-5'>
-        <section className='flex-1 flex flex-col gap-3'>
-        <div className=' flex-1 flex flex-col p-5 pb-7 rounded-lg bg-white'>
-            <h1 className='font-bold ml-2 text-xl mb-3'>Students</h1>
-            <div className=' overflow-hidden border border-1 border-black/10 rounded-lg'>
-                <div className=' bg-black/10 text-black/50 text-xs flex justify-between py-2 px-3'>
-                    <p className='w-10 mr-6 whitespace-nowrap'>Name</p>
-                    <p className='flex-1 text-center'>Progress</p>
-                    <p className='flex-1 text-end'>Days Left</p>
-                </div>
-                <div className='flex flex-col gap-2 p-3'>
-                {sps&&prg?.length>0&&daysL?.length>0?
-                    sps.map((i,index)=>(
-                    <div key={index} onClick={()=>{selectStud(i.SiD)}} className={`
+	React.useEffect(() => {
+		!sps && getStuff();
+		if (!prg || prg.length < 1) {
+			getPrg();
+		}
+		if (!daysL || daysL.length < 1) {
+			getDays();
+		}
+		// console.log({sps,prg,daysL})
+	}, [getProg, getDays, sps, prg]);
+
+	React.useEffect(() => {
+		const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
+		if (highlightedDay && startandendDate && selsectedStudentData) {
+			const x = {
+				weekNum: Math.ceil(
+					(differenceInBusinessDays(
+						highlightedDay,
+						startandendDate.start
+					) +
+						1) /
+						5
+				),
+				dayName: days[parseJSON(highlightedDay).getDay() - 1],
+			};
+
+			try {
+				let y =
+					selsectedStudentData.WEEKLY_PROGRESS[`Week_${x.weekNum}`][
+						`${x.dayName}`
+					];
+				setDayEntryExists(true);
+			} catch {
+				setDayEntryExists(false);
+			}
+			setWhatWeek(x);
+		}
+	}, [highlightedDay]);
+	return (
+		<div className="mt-10 flex gap-3 px-5">
+			<section className="flex-1 flex flex-col gap-3">
+				<div className=" flex-1 flex flex-col p-5 pb-7 rounded-lg bg-white">
+					<h1 className="font-bold ml-2 text-xl mb-3">Students</h1>
+					<div className=" overflow-hidden border border-1 border-black/10 rounded-lg">
+						<div className=" bg-black/10 text-black/50 text-xs flex justify-between py-2 px-3">
+							<p className="w-10 mr-6 whitespace-nowrap">Name</p>
+							<p className="flex-1 text-center">Progress</p>
+							<p className="flex-1 text-end">Days Left</p>
+						</div>
+						<div className="flex flex-col gap-2 p-3">
+							{sps && prg?.length > 0 && daysL?.length > 0 ? (
+								sps.map((i, index) => (
+									<div
+										key={index}
+										onClick={() => {
+											selectStud(i.SiD);
+										}}
+										className={`
                      flex items-center justify-between hover:outline-[#FF4A1C]/50
                      outline outline-2 outline-transparent cursor-pointer 
                     transition-[outline] duration-200 rounded-sm py-1  
-                    ${selsectedStudent === i.SiD?"bg-[#FF4A1C] text-white":"bg-white hover:bg-[#FF4A1C]/5"}
-                    `}> 
-                        <div className='mr-6'><Initials name={i.name}/></div>
-                        <div className='flex-1'><ProgressBar selected={selsectedStudent === i.SiD?true:false} prg={prg[index][i.SiD]}/></div>
-                        <p className='flex-1 text-end'>{daysL[index][i.SiD]}</p>
-                    </div>
-                )):<div className=' w-full h-32 flex flex-col justify-center font-bold text-lg items-center'>
-                    <AiOutlineLoading3Quarters className=' animate-spin' color='#FF4A1C'/>
-                    <p className=' text-black/50'>No students have been assigned<br />to you at this moment</p></div>}
-                </div>
-            </div>
-        </div>
-        <div className=' flex-1 flex flex-col p-5 pb-7 rounded-lg bg-white'>
-            <ChakraProvider theme={CalendarTheme}>
-                <Calendar disableWeekends  highlightedDay={highlightedDay?highlightedDay:undefined} singleDateSelection  value={startandendDate?startandendDate:{}} weekStartsOn={1} onSelectDate={(x:CalendarDate|any)=>{console.log(x);setHighlightedDay(x)}}>
-                    <CalendarControls>
-                        <CalendarPrevButton />
-                        <CalendarNextButton />
-                    </CalendarControls>
-                    <CalendarMonths>
-                        <CalendarMonth>
-                            <CalendarMonthName />
-                            <CalendarWeek />
-                            <CalendarDays  />
-                        </CalendarMonth>
-                    </CalendarMonths>
-                </Calendar>
-            </ChakraProvider>
-        </div>
-        </section>
-        <section className='flex-[2] justify-stretch flex-row md:flex-col pb-5'>
-            <div className='bg-white h-full p-5 rounded-lg'>
-                {!selsectedStudentData
-                    ?<div className='w-full h-full flex justify-center items-center font-black text-5xl text-black/20 uppercase'>
-                        Select a student to view <br /> data here
-                    </div>
-                    :<div className='flex flex-col gap-4'>
-                        <div>
-                            <h1 className='font-bold text-xl'>
-                                {sps?.find(x=>x.SiD == selsectedStudent).name}
-                            </h1>
-                            <p className='text-sm text-black/50'>
-                                {selsectedStudentData.PARTICULARS.registrationNumber}
-                            </p>
-                            <p className='text-sm text-black/50'>
-                                {selsectedStudentData.WEEKLY_PROGRESS?selsectedStudentData.WEEKLY_PROGRESS.Week_1.friday:"none"}
-                            </p>
-                        </div>
-                      
-                    </div>
-                }
-            </div>
-        </section>
-        </div>
-    )
+                    ${
+						selsectedStudent === i.SiD
+							? 'bg-[#FF4A1C] text-white'
+							: 'bg-white hover:bg-[#FF4A1C]/5'
+					}
+                    `}>
+										<div className="mr-6">
+											<Initials name={i.name} />
+										</div>
+										<div className="flex-1">
+											<ProgressBar
+												selected={
+													selsectedStudent === i.SiD
+														? true
+														: false
+												}
+												prg={prg[index][i.SiD]}
+											/>
+										</div>
+										<p className="flex-1 mr-2 text-end">
+											{daysL[index][i.SiD]}
+										</p>
+									</div>
+								))
+							) : (
+								<div className=" w-full h-32 flex flex-col justify-center font-bold text-lg items-center">
+									<AiOutlineLoading3Quarters
+										className=" animate-spin"
+										color="#FF4A1C"
+									/>
+									<p className=" text-black/50">
+										No students have been assigned
+										<br />
+										to you at this moment
+									</p>
+								</div>
+							)}
+						</div>
+					</div>
+				</div>
+				<div className=" flex-1 flex flex-col p-5 pb-7 rounded-lg bg-white">
+					<ChakraProvider theme={CalendarTheme}>
+						<Calendar
+							disableWeekends
+							highlightedDay={
+								highlightedDay ? highlightedDay : undefined
+							}
+							singleDateSelection
+							value={startandendDate ? startandendDate : {}}
+							weekStartsOn={1}
+							onSelectDate={(x: CalendarDate | any) => {
+								console.log(x);
+								setHighlightedDay(x);
+							}}>
+							<CalendarControls>
+								<CalendarPrevButton />
+								<CalendarNextButton />
+							</CalendarControls>
+							<CalendarMonths>
+								<CalendarMonth>
+									<CalendarMonthName />
+									<CalendarWeek />
+									<CalendarDays />
+								</CalendarMonth>
+							</CalendarMonths>
+						</Calendar>
+					</ChakraProvider>
+				</div>
+			</section>
+			<section className="flex-[2] justify-stretch flex-row md:flex-col pb-5">
+				<div className="bg-white h-full p-5 rounded-lg">
+					{!selsectedStudentData ? (
+						<div className="w-full h-full flex justify-center items-center font-black text-5xl text-black/20 uppercase">
+							Select a student to view <br /> data here
+						</div>
+					) : (
+						<div className="flex flex-col gap-4">
+							<div>
+								<h1 className="font-bold text-xl">
+									{
+										sps?.find(x =>
+											x.SiD
+												? x.SiD == selsectedStudent
+												: false
+										).name
+									}
+								</h1>
+								<p className="text-sm text-black/50">
+									{
+										selsectedStudentData.PARTICULARS
+											.registrationNumber
+									}
+								</p>
+								<p className="text-sm text-black/50">
+									{selsectedStudentData.WEEKLY_PROGRESS &&
+									whatWeek &&
+									dayEntryExists
+										? selsectedStudentData.WEEKLY_PROGRESS[
+												`Week_${whatWeek.weekNum}`
+										  ][`${whatWeek.dayName}`]
+										: 'none'}
+								</p>
+							</div>
+						</div>
+					)}
+				</div>
+			</section>
+		</div>
+	);
 }
